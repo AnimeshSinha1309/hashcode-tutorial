5cc51a28f860743f873bdea1ba2437ef6242b416
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2021-02-11 15:22:32 +0530

    Make Python file cleaner

--- greedy_graph.py
+++ greedy_graph.py
@@ -1,15 +1,19 @@
 import random
+import sys
 
 import utils
 
 input_files = ["a_example", "b_lovely_landscapes",
                "c_memorable_moments", "d_pet_pictures", "e_shiny_selfies"]
-file = input_files[0]
-alignments, tags = utils.read_input(file)
+
+filename = input_files[2]
+alignments, tags = utils.read_input(filename)
 
 TRIES_VERTICAL_MATCH = 100
 TRIES_HORIZONTAL_MATCH = 100
 
+SEED = sys.argv[1] if len(sys.argv) > 1 else 42
+random.seed(SEED) # reproducible runs
 
 horizontal_images, vertical_images = list(), list()
 
@@ -18,6 +22,9 @@ for idx, alignment in enumerate(alignments):
         horizontal_images.append(idx)
     elif alignment == 'V':
         vertical_images.append(idx)
+
+# mutable reference used later
+slideshow_slides = horizontal_images
 
 # Combine the Vertical Images
 
@@ -31,26 +38,26 @@ while len(vertical_images) >= 2:
         if trying_score > match_score:
             match_score = trying_score
             chosen_match = trying_match
-    horizontal_images.append((chosen_element, chosen_match))
+    slideshow_slides.append((chosen_element, chosen_match))
     vertical_images.remove(chosen_match)
 
 # Arrange all the images
 
-solution = [random.choice(horizontal_images)]
-horizontal_images.remove(solution[0])
+solution = [random.choice(slideshow_slides)]
+slideshow_slides.remove(solution[0])
 
 while len(horizontal_images) >= 1:
     chosen_element = solution[-1]
     chosen_match, match_score = None, -100000
     for _ in range(TRIES_HORIZONTAL_MATCH):
-        trying_match = random.choice(horizontal_images)
+        trying_match = random.choice(slideshow_slides)
         trying_score = utils.slide_score(tags, chosen_element, trying_match)
         if trying_score > match_score:
             match_score = trying_score
             chosen_match = trying_match
     solution.append(chosen_match)
-    horizontal_images.remove(chosen_match)
+    slideshow_slides.remove(chosen_match)
 
 
-print("Total Score for %s: %s" % (file, utils.score(file, solution)))
-utils.submit(file, solution)
+print("Total Score for %s: %s" % (filename, utils.score(filename, solution)))
+utils.submit(filename, solution)
