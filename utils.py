3204329db894e23908ef6ef9f0a67d69691f58b3
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2021-02-11 15:37:14 +0530

    Minor readability+optimization improvements

--- utils.py
+++ utils.py
@@ -1,20 +1,20 @@
 import os
 
 
-def read_input(file):
-    file = "input/" + file + ".txt"
+def read_input(filename):
+    filename = "input/" + filename + ".txt"
     alignment, tags = [], []
-    with open(file, 'r') as f:
+    with open(filename, 'r') as f:
         _n = int(f.readline())
         for line in f.readlines():
             data = list(line.strip().split())
             alignment.append(data[0])
-            tags.append(data[2:])
+            tags.append(set(data[2:]))
     return alignment, tags
 
 
-def score(file, solution):
-    alignment, tags = read_input(file)
+def score(filename, solution):
+    alignment, tags = read_input(filename)
     points = 0
     for slide in solution:
         if type(slide) is int:
@@ -29,24 +29,22 @@ def score(file, solution):
 
 
 def slide_score(tags, u, v):
-    x = set(tags[u]) if type(u) is int else \
-        set(tags[u[0]]) | set(tags[u[1]])
-    y = set(tags[v]) if type(v) is int else \
-        set(tags[v[0]]) | set(tags[v[1]])
+    x = tags[u] if type(u) is int else \
+        tags[u[0]] | tags[u[1]]
+    y = tags[v] if type(v) is int else \
+        tags[v[0]] | tags[v[1]]
     overlap = len(x & y)
     return min(overlap, len(x) - overlap, len(y) - overlap)
 
 
 def overlap_size(tags, u, v):
-    x = set(tags[u])
-    y = set(tags[v])
-    return len(x | y)
+    return len(tags[u] | tags[v])
 
 
-def submit(file, solution):
+def submit(filename, solution):
     os.makedirs("output", exist_ok=True)
-    file = "output/" + file + ".txt"
-    with open(file, 'w') as f:
+    filename = "output/" + filename + ".txt"
+    with open(filename, 'w') as f:
         f.write(str(len(solution)) + "\n")
         for item in solution:
             if type(item) is int:
